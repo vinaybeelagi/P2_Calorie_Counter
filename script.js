622d71fe9676c7311000ade9d014e54453e2d8a7
vinaybeelagi <vinaykumar.b@neoito.com>
2024-01-17 13:54:53 +0530

    Function to clean input string by removing specific characters

--- script.js
+++ script.js
@@ -5,3 +5,8 @@ const addEntryButton = document.getElementById('add-entry');
 const clearButton = document.getElementById('clear');
 const output = document.getElementById('output');
 let isError = false;
+// Step 2: Function to clean input string by removing specific characters
+function cleanInputString(str) {
+    const regex = /[+-\s]/g;
+    return str.replace(regex, '');
+  }
